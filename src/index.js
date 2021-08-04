a764cb45f18ebb43030a422fbc28644efecec74c
JavierGom98 <javier.gomez98@outlook.com>
2021-08-04 16:22:00 -0500

    Add
    Setting Firebase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import './index.css'
+import rootReducer from './store/reducers/rootReducer';
+import { getFirebase, ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirebaseInstance, getFirestore, reduxFirestore } from 'redux-firestore';
+import fbconfig from './config/fbconfig'
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore}))
+),
+  reduxFirestore(fbconfig)
+)
+
+const rrfProps = {
+  firebase,
+  config, fbconfig,
+  dispatch: store.dispatch,
+  createFirebaseInstance
+}
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
