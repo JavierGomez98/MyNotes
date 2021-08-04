4066091a932b436221dbe24460509cbdd9807e9c
JavierGom98 <javier.gomez98@outlook.com>
2021-08-04 15:42:38 -0500

    Add Component
    Form Note

---
+++ src/components/home/Form.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import useInput from '../../hooks/useInput'
+
+const Form = () => {
+
+    const [title, bindTitle, resetTitle] = useInput()
+    const [content, bindContent, resetContent] = useInput()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log({title, content})
+        resetTitle()
+        resetContent()
+    }
+
+    return (
+        <div className="section">
+            <form action="">
+                <h5 className="grey-text">New Note</h5>
+                <div className="input-field">
+                    <input id="note_title" type="text" className="validate" {...bindTitle} />
+                    <label for="note_title">Note Title</label>
+                </div>
+                <div className="input-field">
+                    <textarea id="note_content" className="materializa-textare" {...bindContent} ></textarea>
+                    <label for="note_content">Note Content</label>
+                </div>
+                <button className="btn blue">Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
