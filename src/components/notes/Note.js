81c54f91ba530386b52ae684e4baae27df79ae38
JavierGom98 <javier.gomez98@outlook.com>
2021-08-05 18:09:49 -0500

    Add
    Delete Notes

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { deleteNote } from '../../store/actions/noteAction'
 
 const Note = ({ note }) => {
+
+    const dispatch = useDispatch()
+    const deleteNoteHandler = () => {
+        dispatch(deleteNote(note))
+    }
+    
     return (
         <div className="note white">
             <div className="right-align">
                 <i className="material-icons red-text" style={{cursor: 'pointer'}}>favorite</i>
-                <i className="material-icons" style={{cursor: 'pointer'}}>delete</i>
+                <i className="material-icons" style={{cursor: 'pointer'}} onClick={deleteNoteHandler}>delete</i>
             </div>
             <h5 className="black-text">{note.title}</h5>
             <p className="truncate">{note.content}</p>
--- src/store/actions/noteAction.js
+++ src/store/actions/noteAction.js
@@ -1,17 +1,44 @@
 export const addNote = (note) => {
-    return (dispatch, getState, {getFirestore}) => {
+    return (dispatch, getState, { getFirestore }) => {
         const firestore = getFirestore()
         firestore.collection('notes')
-        .add({
-            ...note,
-            favorite: false,
-            createdAt: new Date(),
-        })
-        .then(() => {
-            console.log('Add the note Successfully');
-        })
-        .catch((err) => {
-            console.log(err)
-        });
+            .add({
+                ...note,
+                favorite: false,
+                createdAt: new Date(),
+            })
+            .then(() => {
+                console.log('Add the note Successfully');
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+    }
+}
+
+export const deleteNote = (note) => {
+    return (dispatch, getState, { getFirestore }) => {
+        const firestore = getFirestore()
+        firestore.collection('notes').doc(note.id).delete()
+            .then(() => {
+                console.log('Delete the note Successfully');
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+    }
+}
+
+export const toggleFav = (note) => {
+    return (dispatch, getState, { getFirestore }) => {
+        const favorite = !note.favorite
+        const firestore = getFirestore()
+        firestore.collection('notes').doc(note.id).delete()
+            .then(() => {
+                console.log('Delete the note Successfully');
+            })
+            .catch((err) => {
+                console.log(err)
+            });
     }
 }
